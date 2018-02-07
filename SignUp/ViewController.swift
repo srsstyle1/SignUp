5f5dc8c2a9bd776cda80093df1b31e5337a11184
srsstyle <sagarbiswal.job@gmail.com>
2018-02-07 11:02:19 +0530

    Initial Commit

---
+++ SignUp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SignUp
+//
+//  Created by Sagar on 18/11/1939 Saka.
+//  Copyright © 1939 Sadyogi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
